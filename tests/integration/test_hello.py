fbd12bb1426a06d5bbfd55bd659fd2df6c442a43
Aleksandr Dezhin <me@dezhin.net>
2025-10-26 04:49:22 +0300

    Adapt test to hello changes

--- tests/integration/test_hello.py
+++ tests/integration/test_hello.py
@@ -5,7 +5,7 @@ def test_hello(toolbox_client: ToolboxClient):
     hello = toolbox_client.tool("hello")
     result = hello({"name": "Natalia"})
 
-    expected = "Hello, Natalia!"
-    assert result.outputs[0]["value"] == expected
-    assert result["hello"] == expected
-    assert result.value == expected
+    value = result.outputs[0]["value"]
+    assert value.endswith(", Natalia!")
+    assert result["result"] == value
+    assert result.value == value
